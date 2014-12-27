1ac85e1b2f4047be191c21ba15ce63a34202a865
Thuy Trinh <thuy@skedgo.com>
2014-12-27 16:26:56 +0700

    Tap outside to dismiss the picker

--- App/src/main/java/com/thuytrinh/quotemaker/ColorPickerFragment.java
+++ App/src/main/java/com/thuytrinh/quotemaker/ColorPickerFragment.java
@@ -27,6 +27,15 @@ public class ColorPickerFragment extends BaseFragment implements ForViewModel<Co
   public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
+    View overlayView = view.findViewById(R.id.overlayView);
+    overlayView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        // What if this fragment isn't on top of back stack?
+        getFragmentManager().popBackStack();
+      }
+    });
+
     ThemesView colorsView = (ThemesView) view.findViewById(R.id.colorsView);
     colorsView.themes.setValue(viewModel.getThemes());
   }
